e4a2fabd302b0809e66fb51f67f1e1dc39acdb3a
VuDiep1306 <vudiep.1306@gmail.com>
2025-11-14 00:35:51 +0900

    Add files via upload

---
+++ session4_1.sql
@@ -0,0 +1,16 @@
+CREATE DATABASE STUDENTS;
+USE STUDENTS;
+
+CREATE TABLE Students (
+StudentID INT PRIMARY KEY,
+Name VARCHAR (100),
+Age INT,
+Major VARCHAR(100)
+);
+INSERT INTO Students (StudentID, Name, Age, Major)
+VALUES
+(1, 'Alice', 20, 'Computer Science'),
+(2, 'Bob', 22, 'Mathematics'),
+(3, 'Charlie', 21, 'Physics');
+
+SELECT * FROM Students;
